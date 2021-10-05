776c516b5743b342401bdbc493304100b915918f
Tejaswini Ekkaldevi <tejaswini.ekkaldevi@indexnine.com>
2021-10-05 19:53:53 +0530

    Classes and Interfaces  done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -36,3 +51,116 @@ var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2
 console.log(hobbies, hobby1, hobby2);
 var firstName = person.uname, age = person.age;
 console.log(firstName, age, person);
+//Classes and Interfaces
+var Department = /** @class */ (function () {
+    function Department(id, n) {
+        this.id = id;
+        this.name = n;
+    }
+    //Static Methods and properties
+    Department.createEmployee = function (name) {
+        return { name: name };
+    };
+    //constructor function
+    Department.prototype.describe = function () {
+        console.log("Department (" + this.id + ") : " + this.name);
+    };
+    return Department;
+}());
+var employee1 = Department.createEmployee('Shri');
+console.log(employee1);
+//Inheritance
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, 'IT') || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var cse = new ITDepartment('d1', ['max']);
+cse.describe();
+var cseCopy = { name: 'Dummy', describe: cse.describe };
+//cseCopy.describe();
+//private and public Access Modifiers
+var Employee = /** @class */ (function () {
+    function Employee(id, n) {
+        this.employees = [];
+        this.id = id;
+        this.name = n;
+    }
+    //constructor function
+    Employee.prototype.describe = function () {
+        console.log("Department (" + this.id + ") : " + this.name);
+    };
+    Employee.prototype.addEmployee = function (emp) {
+        this.employees.push(emp);
+    };
+    Employee.prototype.printEmpDetails = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Employee;
+}());
+var cse1 = new Employee('d2', 'Computer Science');
+cse1.addEmployee('Jhon');
+cse1.addEmployee('Smith');
+//cse1.employees[2] = 'anna';
+cse1.describe();
+cse1.name = 'Nae name';
+var cseCopy1 = { name: 'Dummy', describe: cse.describe };
+// cseCopy1.describe();
+cse1.printEmpDetails();
+// Overriding Properties and protected modifier
+var Accounting = /** @class */ (function (_super) {
+    __extends(Accounting, _super);
+    //Singletones or private constructors
+    function Accounting(id, reports) {
+        var _this = _super.call(this, id, 'Accounting') || this;
+        _this.reports = reports;
+        return _this;
+    }
+    Accounting.getInstance = function () {
+        if (Accounting.instance) {
+            return this.instance;
+        }
+        this.instance = new Accounting('d3', []);
+        return this.instance;
+    };
+    Accounting.prototype.addEmployee = function (name) {
+        if (name = 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    };
+    Accounting.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    Accounting.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return Accounting;
+}(Employee));
+//const account = new Accounting('d2', []);
+var account = Accounting.getInstance();
+console.log(account);
+account.addReport('Something went wrong');
+account.printReports();
+var Person = /** @class */ (function () {
+    function Person(n) {
+        this.name = n;
+    }
+    Person.prototype.greet = function (phrase) {
+        console.log(phrase + ' ' + this.name);
+    };
+    return Person;
+}());
+var user;
+user = new Person('Raj');
+user.greet('Hello, I am ');
+var addFn;
+addFn = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(addFn(4, 4));
--- src/app.ts
+++ src/app.ts
@@ -31,3 +31,164 @@ const {uname: firstName, age} = person;
 console.log(firstName, age, person);
 
 
+//Classes and Interfaces
+
+class Department {
+    id: string;
+    name: string;
+
+    constructor(id:string, n: string) {
+        this.id = id;
+        this.name = n ;
+    }
+
+    //Static Methods and properties
+    static createEmployee(name: string){
+        return{name: name};
+    }
+    //constructor function
+    describe(this: Department) {
+       console.log(`Department (${this.id}) : ${this.name}`)
+    } 
+}
+
+const employee1 = Department.createEmployee('Shri');
+console.log(employee1);
+//Inheritance
+
+class ITDepartment extends Department {
+    admins: string[];
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
+
+const cse = new ITDepartment('d1',['max']);
+cse.describe();
+
+const cseCopy = {name: 'Dummy', describe:cse.describe};
+//cseCopy.describe();
+
+
+
+//private and public Access Modifiers
+
+class Employee {
+    id: string;
+    name: string;
+    protected employees: string[] = [];
+
+    constructor(id:string, n: string) {
+        this.id = id;
+        this.name = n ;
+    }
+    //constructor function
+    describe(this: Employee) {
+        console.log(`Department (${this.id}) : ${this.name}`)
+    } 
+    addEmployee (emp: string) {
+        this.employees.push(emp);
+    }
+    printEmpDetails() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const cse1 = new Employee('d2', 'Computer Science');
+cse1.addEmployee('Jhon');
+cse1.addEmployee('Smith');
+//cse1.employees[2] = 'anna';
+
+cse1.describe();
+cse1.name = 'Nae name';
+const cseCopy1 = {name: 'Dummy', describe:cse.describe};
+// cseCopy1.describe();
+cse1.printEmpDetails();
+
+// Overriding Properties and protected modifier
+
+class Accounting extends Employee {
+    private static  instance: Accounting;
+    //Singletones or private constructors
+     private constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+    }
+    static getInstance() {
+        if(Accounting.instance){
+            return this.instance;
+        }
+        this.instance = new Accounting('d3', []);
+        return this.instance;
+    }
+
+    addEmployee(name: string) {
+        if(name = 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    printReports(){
+        console.log(this.reports);
+    }
+}
+
+//const account = new Accounting('d2', []);
+const account = Accounting.getInstance();
+console.log(account);
+account.addReport('Something went wrong');
+account.printReports(); 
+
+
+// Interface
+//& Using Interfaces with class 
+interface Named {
+    readonly name: string;
+    //Optional property indicated using the '?'
+    outputName?: string;
+}
+//Extending interfaces
+
+interface Greetable extends Named {
+    greet(phrase: string):void;
+}
+class Person implements Greetable {
+    name:string;
+    age: 30;
+    constructor(n: string) {
+        this.name = n;
+    }
+    greet(phrase: string){
+        console.log(phrase + ' ' + this.name);      
+    }
+}
+
+let user: Greetable;
+user = new Person('Raj');
+user.greet('Hello, I am ');
+/*
+ user = {
+     name:'Raj',
+    age: 25;
+     greet(phrase: string) {
+         return(phrase + ' ' + this.name);
+     }
+ }
+ console.log(user.greet('Hello, I am '));
+ */
+
+// Interfaces as function Types
+interface AddFn {
+    (a: number, b: number): number;
+}
+let addFn: AddFn;
+addFn = (n1: number, n2: number) => {
+    return n1 + n2;
+};
+console.log(addFn(4,4));
