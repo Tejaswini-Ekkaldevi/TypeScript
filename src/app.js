5d06ce335ffc581d544f8aa4ac7462b036143f4b
Tejaswini Ekkaldevi <tejaswini.ekkaldevi@indexnine.com>
2021-10-05 15:24:57 +0530

    Next-gen javascript and typescript done

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+// spread operator
 var hobbies = ['sports', 'cooking'];
 var activehobby = ['travelling'];
 activehobby.push.apply(activehobby, hobbies);
@@ -18,3 +19,20 @@ var person = {
     age: 20
 };
 var copiedPerson = __assign({}, person);
+// Rest parameter
+var add = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (curRes, curVal) {
+        return curRes + curVal;
+    }, 0);
+};
+var addnumbers = add(10, 25, 4.5, 6);
+console.log(addnumbers);
+// Arrays and Object Destructuring
+var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
+console.log(hobbies, hobby1, hobby2);
+var firstName = person.uname, age = person.age;
+console.log(firstName, age, person);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// spread operator
 const hobbies = ['sports', 'cooking'];
 const activehobby = ['travelling'];
 
@@ -10,3 +11,23 @@ const person = {
     age : 20
 }
 const copiedPerson = {...person};
+
+// Rest parameter
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curRes, curVal) => {
+        return curRes + curVal;
+    },0);
+}
+const addnumbers = add(10, 25, 4.5, 6);
+console.log(addnumbers);
+
+// Arrays and Object Destructuring
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+const {uname: firstName, age} = person;
+console.log(firstName, age, person);
+
+
